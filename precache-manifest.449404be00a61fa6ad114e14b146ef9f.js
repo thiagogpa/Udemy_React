e2853b72e21d4d42e818a1f9eb9dbaa29f814e74
Thiago Andrade <thiagogpa@gmail.com>
2020-06-11 21:12:47 -0400

    Updates

---
+++ precache-manifest.449404be00a61fa6ad114e14b146ef9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ea0337b62e0d68b6acec744fe0c9d7",
+    "url": "/react_monsters/index.html"
+  },
+  {
+    "revision": "2a5d99489076f54062af",
+    "url": "/react_monsters/static/css/main.b0f3a354.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/react_monsters/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_monsters/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5d99489076f54062af",
+    "url": "/react_monsters/static/js/main.c779154d.chunk.js"
+  },
+  {
+    "revision": "88dfea55b8bd84b2dfbf",
+    "url": "/react_monsters/static/js/runtime-main.aae4c151.js"
+  }
+]);
